966d0fb6a45d8bccbc2255223752aef556e0647f
Armen Nagapetian <armen.nagapetian@gmail.com>
2021-01-24 16:04:31 -0800

    Added Result page
    Added Result Page and added some extra logic on scraping

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import * as $ from 'jquery'
 import { nextTick } from 'process';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
 import { forkJoin } from 'rxjs/internal/observable/forkJoin';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as $ from 'jquery'
 
 
 
@@ -21,18 +21,19 @@ export class HomeComponent implements OnInit {
   searchterm:any;
   HomeisActive:boolean = true;
   ResultisActive:boolean = false;
+  ResultisActive2:boolean = true;
   loadedCharacter: {};
-  Url:string = "test";
+  Url:string;
   constructor(private http: HttpClient,) { }
 
   ngOnInit() {}
 
 
   search(){
-    
     let subscriptionKey = this.apikey;
     let customConfigId = 'YOUR-CUSTOM-CONFIG-ID';
-    let searchTerm = 'inbody:"parent directory"'+ this.searchterm +'';
+    let searchTerm = 'intitle:"index of" contains:(mkv mp4 avi mov mpg wmv divx mpeg)' + this.searchterm;
+    //let searchTerm = this.searchterm +'(mkv|mp4|avi|mov|mpg|wmv|divx|mpeg) -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml) intitle:index.of -inurl:(listen77|mp3raid|mp3toss|mp3drug|index_of|index-of|wallywashis|downloadmana)';
 
     let headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -41,7 +42,6 @@ export class HomeComponent implements OnInit {
    let options = {
       headers: headers
    }
-
     // here your url
     let url = 'https://api.bing.microsoft.com/v7.0/search?' +
       'q=' + searchTerm + '&count=45';
@@ -57,6 +57,20 @@ export class HomeComponent implements OnInit {
 
   }
 
-  
+  loadpage(url){
+    this.Url = url;
+    this.HomeisActive = false;
+    this.ResultisActive = true;
+  }
+
+  back1(){
+    if(!this.HomeisActive){
+      this.HomeisActive = true;
+      this.ResultisActive = false;
+    }
+
+  }
+
+
 
 }
--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,18 +1,51 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import * as $ from 'jquery'
+import * as cheerio from 'cheerio';
+
+
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  styleUrls: ['./result.component.css'],
 })
 export class ResultComponent implements OnInit {
+  @Input() Url: string;
+  links: any;
+  titles: any;
+  constructor() {}
 
-  constructor() { }
-
-  @Input() Url:string;
-
-  ngOnInit(){
-    console.log(this.Url);
+  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges) {
+    this.load();
   }
 
+
+  load() {
+    let links = [];
+    let titles = [];
+    let original = this.Url;
+    if (this.Url) {
+      $.ajax({
+        url: this.Url,
+      }).done(function (data) {
+        let fixed = data.replaceAll('href="', 'href="' + original);
+        //let a = $(fixed).n('a').attr('href');
+        // console.log(a);
+        // $(fixed).find("a").each( function(){
+        //   links.push(this.href);
+        // } )
+        $(fixed)
+          .find('a')
+          .each(function () {
+            links.push(this.href);
+            titles.push(this.textContent);
+          });
+        console.log(JSON.stringify(titles));
+        console.log(JSON.stringify(links));
+      });
+    }
+    this.links = links;
+    this.titles = titles;
+  }
 }
